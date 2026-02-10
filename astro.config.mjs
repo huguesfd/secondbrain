264d34469461c1976ca37b42143f33a9be4a4127
Hugues-kiln <hugues.ferrand@kiln.fi>
2026-02-10 23:48:31 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://<TON_GITHUB_USERNAME>.github.io",
-  base: "/your-second-brain"
+  site: "https://huguesfd.github.io",
+  base: "/secondbrain"
 });
